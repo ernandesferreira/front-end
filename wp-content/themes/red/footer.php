5ace8ac48b658edf00dac0482db47cf7315cc1ba
ernandesferreira <ernandes.dev@gmail.com>
2016-07-25 15:53:55 -0300

    finalizando html css

--- wp-content/themes/red/footer.php
+++ wp-content/themes/red/footer.php
@@ -14,9 +14,62 @@
 	</div><!-- #wrapper -->
 
 	<footer id="footer" role="contentinfo">
-		<div class="container">
-			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
-		</div><!-- .container -->
+		<div class="footer-home">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 footer1">
+						<?php dynamic_sidebar( 'footer-1' ); ?>
+					</div>
+					<div class="col-md-1 footer2">
+						<?php dynamic_sidebar( 'footer2' ); ?>
+					</div>
+					<div class="col-md-2 parceiros-footer">
+						<h2> Parceiros Globais: </h2>
+						<div class="img-parceiro">
+							<img src="<?php echo get_template_directory_uri().'/assets/images/partner.png'; ?>">
+						</div>
+					</div>
+					<div class="col-md-2 redes-sociais-footer">
+						<h2> Conecte-se </h2>
+						<div class="link-sociais">
+							<ul class="list-inline">
+								<li>
+									<a href="" alt="Facebook" title="Facebook"><img class="facebook" src="<?php echo get_template_directory_uri().'/assets/images/face-footer.png'; ?>"></a>
+								</li>
+								<li>
+									<a href="" alt="Linkedin" title="Linkedin"><img class="facebook" src="<?php echo get_template_directory_uri().'/assets/images/linkedin-footer.png'; ?>"></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-4 sedes-footer">
+						<div class="sedes">
+							<ul class="list-inline">
+								<li>
+									<img src="<?php echo get_template_directory_uri().'/assets/images/sp.svg'; ?>" alt="São Paulo" title="São Paulo">
+									<div class="name-sede"> São Paulo </div>
+								</li>
+								<li>
+									<img src="<?php echo get_template_directory_uri().'/assets/images/rj.svg'; ?>" alt="Rio de Janeiro" title="Rio de Janeiro">
+									<div class="name-sede"> Rio de Janeiro </div>
+								</li>
+								<li>
+									<img src="<?php echo get_template_directory_uri().'/assets/images/ctb.svg'; ?>" alt="Curitiba" title="Curitiba">
+									<div class="name-sede"> Curitiba </div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>	
+					<div class="row">
+						<div class="col-md-3"> </div>
+						<div class="col-md-9 line-footer"> 
+							<div class="copyright"> &reg Red Consultng </div>
+						</div>
+					</div>
+								
+			</div><!-- .container -->
+		</div>
 	</footer><!-- #footer -->
 
 	<?php wp_footer(); ?>
--- wp-content/themes/red/front-page.php
+++ wp-content/themes/red/front-page.php
@@ -83,29 +83,110 @@ get_header(); ?>
 			</div>
 		</div>	
 	</main><!-- #content -->
-</div>
-</div>
-	<div class="container-fluid line-news">
-		<div id="wrapper" class="container">
-			<div class="row">	
-				<div class="col-md-6">
-					<div class="red-destaque">
-						<h2> Red em Destaque </h2>
+</div><!-- #row header -->
+</div><!-- #wrapper header -->
+<div class="container-fluid">
+	<div class="row line-news">	
+		<div class="col-md-6">
+			<div class="red-destaque">
+				<h2> Red em Destaque </h2>
+			</div>
+		</div>				
+		<div class="col-md-4">
+			<div class="red-news">
+				<h2> Notícias </h2>
+			</div>
+		</div>
+		<div class="col-md-2">
+			<div class="red-more">
+				<a href="" title="Ver Todas as notícias"> Ver todas </a>
+			</div>
+		</div>		
+	</div>
+		<div class="row">
+			<!-- coluna destaque -->
+			<div class="col-md-6">	
+
+					<div class="container-destaque">
+						<div class="box-destaque">
+							<div class="img-destaque">
+								<img src="<?php echo get_template_directory_uri().'/assets/images/img-noticia.jpg'; ?>">
+							</div>
+							<div class="content-news-destaque">
+								<div class="date-destaque">
+									30/9/2014
+								</div>
+								<div class="title-destaque">
+									<h3>
+										<a href="#">
+										Para vencer na carreira há preços a pagar, diz Luiza Trajano
+										</a>
+									</h3>
+								</div>
+								<div class="description-news-destaque">
+									<p>
+										São Paulo - Luiza Helena Trajano começou a sua carreira na área de
+										vendas, aos 12 anos, quando abriu mão das férias escolares para
+										futures as solar and win met a record share of the country's demand 
+										last year.
+									</p>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="col-md-4">
-					<div class="red-news">
-						<h2> Notícias </h2>
+				
+			</div>
+					<!-- coluna noticias -->
+					<div class="col-md-6 collun-news-all">						
+							<div class="content-news-all">
+								<div class="box-content-all">
+									<ul>
+										<li>
+											<div class="date-news-all">
+												30/9/2014
+											</div>
+											<div class="title-news-all">
+												<h3>
+													<a href="#">
+														Nasdaq OMX Plans German renewable
+														Power futures in 2015
+													</a>
+												</h3>
+											</div>
+											<div class="description-news-all">
+												<p>
+													Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius imperdiet pharetra. Nam in tempus orci, eget mattis nunc. Praesent ac maximus metus. Duis vel urna eget urna porta venenatis vehicula at est. In sit amet felis quis velit mollis ornare. Aliquam vestibulum sem elit. In rhoncus viverra libero eleifend porttitor. Aenean 
+												</p>
+											</div>
+										</li>
+										<li>
+											<div class="date-news-all">
+												30/9/2014
+											</div>
+											<div class="title-news-all">
+												<h3>
+													<a href="#">
+														Nasdaq OMX Plans German renewable
+														Power futures in 2015
+													</a>
+												</h3>
+											</div>
+											<div class="description-news-all">
+												<p>
+													Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent varius imperdiet pharetra. Nam in tempus orci, eget mattis nunc. Praesent ac maximus metus. Duis vel urna eget urna porta venenatis vehicula at est. In sit amet felis quis velit mollis ornare. Aliquam vestibulum sem elit. In rhoncus viverra libero eleifend porttitor. Aenean 
+												</p>
+											</div>
+										</li>		
+									</ul>	
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="col-md-2">
-					<div class="red-more">
-						<span> Ver todas </span>
-					</div>
-				</div>
+				
 			</div>
 		</div>
 	</div>	
+</div>	
 <div id="wrapper" class="container">
 	<div class="row">	
 
